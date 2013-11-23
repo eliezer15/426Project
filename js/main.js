38e363e6b6f27f07fa148f1578d5023e23fc1d0e
boblu27 <boblu27@yahoo.com>
2013-11-23 12:43:22 -0500

    updated main with lightbox

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ $(document).ready(function() {
 	for(var i=0; i<10; i++) {
 		var image = imageObj.all.pop();
 	var new_imagediv = $('<div class ="pic' + image.id  +'"></div>');
-	new_imagediv.append('<img src="'+ image.url +'"><br><img class ="thumbup" src="images/thumbup.png"><img class ="thumbdn" src="images/thumbdown.png">'
+	var lblink = $('<a href="' + image.url +'" data-lightbox="image-1" title="My caption"></a>');
+	lblink.append('<img src="'+ image.url +'"><br><img class ="thumbup" src="images/thumbup.png"><img class ="thumbdn" src="images/thumbdown.png">'
 		+'</div>');
+	new_imagediv.append(lblink);
 	
 	imageScroll.append(new_imagediv);
 	}
