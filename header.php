b0b52a85c7873e7a2f723aecc4766d7233bac99f
boblu27 <boblu27@yahoo.com>
2013-11-23 22:37:25 -0500

    Updated site layout colors

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<p> Welcome, <?php if (isset($_SESSION['login'])){echo $_SESSION['user']; echo '! <a href="logout.php">click here to logout</a>';} else echo 'Guest <a href="login.php">login</a>!<a href="signup.php" id="register">Register Now!</a>'; ?></p>
+<p><div class ="head">Welcome, <?php if (isset($_SESSION['login'])){echo $_SESSION['user']; echo '! <a href="logout.php">click here to logout</a>';} else echo 'Guest <a href="login.php">login</a>!<a href="signup.php" id="register">Register Now!</a>'; ?></p></div>
 	<div class="logo">
 		<h1> Tar Heel Gallery </h1>
 	</div><!-- logo place holder -->
